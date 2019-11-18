7eaa7500ac032d1ac4cda11865869e8361bd4576
Riley Flynn <technologyflynnteam@gmail.com>
2019-11-18 20:23:17 -0330

    WIP compile script

--- .github/actions/compile-docs/entrypoint.py
+++ .github/actions/compile-docs/entrypoint.py
@@ -1,3 +1,37 @@
-import os
+from typing import List
 
-print(os.listdir(os.getcwd()))
+from pprint import pprint
+
+import pathlib
+import subprocess
+
+MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+minute_files: List[pathlib.Path] = pathlib.Path("/Minutes/").glob("*.md")
+output_path = pathlib.Path("/output")
+
+years = {}
+
+for file_ in minute_files:
+    _, month_number, __, year = file_.stem.split("_")
+    if year not in years:
+        years[year] = {}
+    month = MONTHS[int(month_number) + 1]
+    if month not in years[year]:
+        years[year][month] = []
+    years[year][month].append(file_)
+
+pprint(years)
